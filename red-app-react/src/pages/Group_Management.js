e68979adbaae2b7bb6a72ada78e50b99c270377c
kawasugikazuki <a20.3c7x@g.chuo-u.ac.jp>
2023-09-27 21:30:02 +0900

    show connected redID

--- red-app-react/src/pages/Group_Management.js
+++ red-app-react/src/pages/Group_Management.js
@@ -5,26 +5,43 @@ import { Shutdown } from "../components/shutdown";
 import { Restart } from "../components/Restart";
 import { getIP } from "../components/Get_brokerIP";
 import { Button } from "@mui/material";
+import { get_redID } from "../components/Get_redID";
 
 
 export const Group=()=>{
   const [IP,setIP]=useState("");
     useEffect(()=>{
-        const fetchData=async()=>{
+        const fetchIP=async()=>{
             try{
-                const response=await getIP();
-                setIP(response);
+                const IP=await getIP();
+                setIP(IP);
             }catch(error){
                 console.log(error);
             }
         };
-        fetchData();
+        fetchIP();
     },[]);
-     
+
+    const [ID,setID]=useState([]);
+    useEffect(()=>{
+        const fetchID=async()=>{
+            try{
+                const result=await get_redID();
+                setID(result);
+            }catch(error){
+                console.log(error);
+            }
+        };
+        fetchID();
+    },[ID]);
+
+
         return(
             <div>
                 <h1>**Swarm System Operation and Management**</h1>
                 <h2>Server's IP address :{IP}</h2>
+                <ul>{ID.map((item,index)=>(<li key={index}>{item}</li>))}</ul>
+        
                 <Button variant="outlined" onClick={()=>{BrokerIP(IP)}}>BrokerIP</Button>
                 <Button variant="outlined" onClick={StartExplore}>Start Explore</Button>
                 <Button variant="outlined" onClick={Shutdown}>Shutdown</Button>
