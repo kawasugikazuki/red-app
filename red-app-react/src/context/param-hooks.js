9229c559cac48320e07727aca1849b00b208e93d
kawasugikazuki <a20.3c7x@g.chuo-u.ac.jp>
2023-12-10 22:13:44 +0900

    delete param-data

--- red-app-react/src/context/param-hooks.js
+++ red-app-react/src/context/param-hooks.js
@@ -1,5 +1,4 @@
 import React,{createContext,useState, useContext,useReducer} from "react";
-import {param} from "./param_data";
 
 const ParamContext=createContext();
 const paramReducer=(state,action)=>{
@@ -13,6 +12,25 @@ const paramReducer=(state,action)=>{
             throw state;
     }
 };
+const param = {
+    IsExploring: false,
+    TransitTime: 2.0,
+    Mu:1.0,
+    Sigma: 1.0,
+    Outer_Rth: 3.0,
+    Inner_Rth: 0.0,
+    Height: 2.2,
+    BetweenMarkers: 0.8,
+    Height_Correction: false,
+    Reject: "A",
+    MarkerColor: "",
+    ShutterSpeed: 100,
+    Xcoord: 0.0,
+    Ycoord: 0.0,
+    LeftPWM: 0.0,
+    RightPWM: 0.0
+};
+
 
 export const ParamProvider=({children})=>{
     const [paramState,paramDispatch]=useReducer(paramReducer,param);
